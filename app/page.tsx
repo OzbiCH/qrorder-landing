7696ae4fb9d81bd14c90c70b58324af6341c18d2
Ersan <ozan.ersan@zeiss.com>
2026-08-19 16:51:33 +0200

    Make logo much larger - navigation and splash

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ function ParallaxSplash({ onComplete }: { onComplete: () => void }) {
           marginTop: '-160px',
         }}
       >
-        <div className="w-80 h-80">
+        <div className="w-96 h-96">
           <img 
             src="/logo-primary.png" 
             alt="QR Order" 
